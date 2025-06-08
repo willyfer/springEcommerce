3764216ba756be69756f7e3a427b604291b5ee19
Willy Fernandez <desarrollador.frontend@navitrack.com.pe>
2025-06-08 08:11:47 -0500

    feat: view producthome

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -1,11 +1,15 @@
 package com.curso.ecommerce.controller;
-
+ 
 import java.util.List;
+import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory; 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.curso.ecommerce.model.Product;
@@ -14,6 +18,8 @@ import com.curso.ecommerce.service.ProductService;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	private final Logger LOGGER =  LoggerFactory.getLogger(HomeController.class);
 	@Autowired
 	private ProductService productService;
 	
@@ -23,6 +29,17 @@ public class HomeController {
 		model.addAttribute("products",products);
 		
 		return "user/home";
+	}
+	
+	@GetMapping("/product/{id}")
+	public String productHome(@PathVariable Integer id, Model model) {
+		Product p = new Product();
+		Optional<Product> product = productService.get(id);
+	
+		p= product.get();
+		LOGGER.info("product{}", p);
+		model.addAttribute("product",p);
+		return "user/productohome";
 	}
 
 }
