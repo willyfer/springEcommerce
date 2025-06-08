3dcf32624981af3c2dfd1ce60c55fc8b65214343
Willy Fernandez <desarrollador.frontend@navitrack.com.pe>
2025-06-08 08:20:43 -0500

    feat: view cart

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.curso.ecommerce.model.Product;
@@ -41,5 +42,10 @@ public class HomeController {
 		model.addAttribute("product",p);
 		return "user/productohome";
 	}
-
+	@PostMapping("/cart")
+	public String cart(Model model) {
+	 
+		
+		return "user/carrito";
+	}
 }
